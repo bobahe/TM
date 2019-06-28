33d5c9dfff052ed91a53d79470cba0672efc633e
bobahe@gmail.com <&4Kelly&4>
2019-06-28 17:13:52 +0300

    Created User entity

---
+++ src/main/java/ru/levin/tm/entity/User.java
@@ -0,0 +1,31 @@
+package ru.levin.tm.entity;
+
+public class User {
+    private String login;
+    private String password;
+    private RoleType roleType;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+}
